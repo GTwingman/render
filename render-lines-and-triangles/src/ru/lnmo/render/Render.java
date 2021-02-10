0a73ddc65abb75824dde7527d5597124087dd717
GTwingman <71901008+GTwingman@users.noreply.github.com>
2021-02-10 20:32:32 +0300

    Update Render.java

--- render-lines-and-triangles/src/ru/lnmo/render/Render.java
+++ render-lines-and-triangles/src/ru/lnmo/render/Render.java
@@ -8,10 +8,11 @@ public class Render {
     public static void render(BufferedImage img) {
 
         Vector.tangle(img, 100, 200, 300,200,400,500);
+        //рисование линий
      for (int j = 1; j <= 16; j++) {
             Vector.line(img,500,400,500+Math.cos(j* Math.PI/8 )*100,400+Math.sin(j* Math.PI/8)*100);
      }
-        
+        //рисование треугольников 
       for (int j = 1; j <= 3; j++) {
              img.getGraphics().setColor(Color.BLACK);
              img.getGraphics().drawLine(
