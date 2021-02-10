15b63c871bc27401fb5997e449f83a40fb2e8ebb
GTwingman <71901008+GTwingman@users.noreply.github.com>
2021-02-10 20:31:28 +0300

    Update Render.java

--- render-lines-and-triangles/src/ru/lnmo/render/Render.java
+++ render-lines-and-triangles/src/ru/lnmo/render/Render.java
@@ -11,6 +11,19 @@ public class Render {
      for (int j = 1; j <= 16; j++) {
             Vector.line(img,500,400,500+Math.cos(j* Math.PI/8 )*100,400+Math.sin(j* Math.PI/8)*100);
      }
+        
+      for (int j = 1; j <= 3; j++) {
+             img.getGraphics().setColor(Color.BLACK);
+             img.getGraphics().drawLine(
+                     500, 400,
+                     (int)(500+Math.cos(j* Math.PI*2/3 )*100),(int)(400+Math.sin(j* Math.PI*2/3)*100)
+             );
+                Vector.tanglega(img,
+                        500+Math.cos(j* Math.PI*2/3 )*100,400+Math.sin(j* Math.PI*2/3)*100,
+                        500+Math.cos(j* Math.PI*2/3 )*300,400+Math.sin(j* Math.PI*2/3)*300,
+                        500+Math.cos((j+1)* Math.PI*2/3 )*300, 400+Math.sin((j+1)* Math.PI*2/3)*300,
+                        j);
+         }
 
     }
 
